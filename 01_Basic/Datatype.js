0408fbf535a9b58883f645e6c3878d486389045a
CodeWithRajesh <103139833+RajeshKaliaWeb@users.noreply.github.com>
2026-09-13 05:06:12 +0000

    number

---
+++ 01_Basic/Datatype.js
@@ -0,0 +1,27 @@
+let score = "33qwqeqe";
+// console.log(typeof score);
+// console.log(typeof (score));
+let valuNumber = Number(score);
+// console.log(typeof valuNumber);
+
+/* Number convert
+"33" => 33,
+Null => 0,
+"23fff" => NaN,
+true => 1, false => 0
+*/
+
+let bolan = 0;
+let contBol = Boolean(bolan);
+console.log(contBol);
+
+/*
+0 => false, 1 => true, "" => false, "rajesh" => true
+*/
+
+let stval = 33;
+let valurString = String(stval);
+console.log(valurString);
+console.log(typeof valurString);
+
+
