3c02213632e0cf07f509b609faf9cc23ea8d0b40
CodeWithRajesh <103139833+RajeshKaliaWeb@users.noreply.github.com>
2026-09-13 11:00:36 +0000

    number

---
+++ basic/numberAndMath.js
@@ -0,0 +1,33 @@
+let blanse = new Number(100);
+// console.log(blanse);
+
+// console.log(blanse.toString().length);
+// console.log(blanse.toFixed(2));
+
+let otherNumber = 4.1234;
+// console.log(otherNumber.toPrecision(2));
+
+let hunderd = 1000000;
+// console.log(hunderd.toLocaleString('en-in'));
+
+// ---------------Math------------
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5));
+// console.log(Math.round(4.4));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.5));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor((Math.random()*10) + 1));
+
+const min = 10;
+const max = 25;
+const result = Math.floor(Math.random() * (max - min + 1) + min);
+console.log(result);
+
+
+
+
